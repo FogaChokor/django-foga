e9f53530697f86f3fa5373d979cf813a742a3f89
FogaChokor <fogachokor@yahoo.com>
2023-03-02 10:12:36 +0000

    Project Restructuring

--- itapps/settings.py
+++ itapps/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'itreporting',
+    'itreporting.apps.ItreportingConfig',
+    'users',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -118,7 +120,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+LOGIN_REDIRECT_URL= 'itreporting-home'
+LOGIN_URL = 'login'
+
--- itservices/itapps/itreporting/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('', views.home, name='itreporting'),
-    path('', views.about, name='itreporting'),
-] 
